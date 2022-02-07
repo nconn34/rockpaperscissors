26d482ccbdb909b754b25de2f0063d285f83d7ec
Christen Weston <paleeserecycle@gmail.com>
2022-02-06 16:39:56 -0800

    Sixth test passing

--- rockPaperScissor.Tests/ModelTests/rockPaperScissorsTests.cs
+++ rockPaperScissor.Tests/ModelTests/rockPaperScissorsTests.cs
@@ -83,21 +83,33 @@ namespace RockPaperScissor.Tests
         Assert.AreEqual(newGame.DecideWhoWins(user1, user2), result);
       }
 
-      //      [TestMethod]
-      // public void RockPaperScissor_RockBeatsScissors()
-      // {
-      //   //Arrange
-      //   string user1 = "scissors";
-      //   string user2 = "rock";
+      [TestMethod]
+      public void RockPaperScissor_PaperBeatsRock()
+      {
+        //Arrange
+        string user1 = "paper";
+        string user2 = "rock";
 
-      //   //Act
-      //   string result = "user2 wins!";
-      //   RockPaperScissorGame newGame = new RockPaperScissorGame();
+        //Act
+        string result = "user1 wins!";
+        RockPaperScissorGame newGame = new RockPaperScissorGame();
 
-      //   //Assert
-      //   Assert.AreEqual(newGame.DecideWhoWins(user1, user2), result);
-      // }
+        //Assert
+        Assert.AreEqual(newGame.DecideWhoWins(user1, user2), result);
+      }
+    //   [TestMethod]
+    //   public void RockPaperScissor_PaperBeatsRock()
+    //   {
+    //     //Arrange
+    //     string user1 = "paper";
+    //     string user2 = "rock";
 
-      
+    //     //Act
+    //     string result = "user1 wins!";
+    //     RockPaperScissorGame newGame = new RockPaperScissorGame();
+
+    //     //Assert
+    //     Assert.AreEqual(newGame.DecideWhoWins(user1, user2), result);
+    //   }
     }
 }
